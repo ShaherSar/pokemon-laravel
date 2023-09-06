32fce82486deac42222936c2e6dcf3792fcef3d8
Shaher Sarairah <shahersar@icloud.com>
2023-09-06 16:44:11 +0300

    remove not needed lines

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/test',function (){
-    try {
-
-        return 'done';
-    }catch (\Exception $exception){
-        die($exception->getMessage());
-    }
-});
-
-function stringToKey($string) : string {
-    return trim(str_replace(" ", "_", $string));
-}
